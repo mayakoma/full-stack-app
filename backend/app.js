7bf72abc5f35599a459b4743ccb94c4c40f5642f
maya koma <mayakoma@gmail.com>
2022-06-25 17:08:50 +0300

    changes

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const placesRouter = require("./routers/places-router");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use("/api/places", placesRouter); //=>   /api/places...
+
+app.use((req, res, next) => {
+  const error = new HttpError("Cold  not find this route", 404);
+  throw error;
+});
+
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
@@ -12,6 +21,7 @@ app.use((error, req, res, next) => {
   res.status(error.code || 500);
   res.json({ message: error.message || "An unknown error occurred!" });
 });
+
 app.listen(5000, () => {
   console.log("listen port 5000");
 });
